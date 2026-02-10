9d7f51ac060cfd82ac5f876a87eee4f7f08fc1de
Sourabh Farkare <sourabhfarkare73@gmail.com>
2026-02-10 18:23:27 +0530

    first commit

---
+++ Array_basics.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // create
+    //first way
+    int arr[5]; //by default garbage value hogi aur baad me hum change kar sakte hai
+    // second way
+    arr[0]=1;
+     arr[1]=1;
+      arr[2]=1;
+       arr[3]=1;
+        arr[4]=1;
+
+    int arr_b[]={1,2,3,4,5}; // pehle se intialize hai aur baad me change bhi kar sakte hai
+    // third way
+    int n;
+    cin>>n;
+    int arr_c[n]; // by default garbage and baad me change kar sakte hai
+    // Read
+    int s=sizeof(arr_b)/sizeof(int);
+    for(int i=0;i<s;i++){
+        cout<<arr_b[i]<<" ";
+    }
+    cout<<endl;
+    for(int i=0;i<s;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    for(int i=0;i<s;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
---
+++ code1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n=4;
+    for(int i=1;i<=n;i++){ //outer loop
+        for(int j=1;j<=n;j++){ //inner loop
+        cout<<j << " "; //for space 
+    }
+    cout<<endl;
+}
+    
+    return 0;
+}
