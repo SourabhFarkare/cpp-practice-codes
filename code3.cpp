b895ff6f0d8ba33789ae8567af650938784cc540
Sourabh Farkare <sourabhfarkare73@gmail.com>
2026-02-12 13:51:56 +0530

    new changes

--- code3.cpp
+++ code3.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 int main() { 
-   int n = 9;
+   int n;
+   cin>>n;
      for(int i=0; i<n; i++) { //outer
      char ch='A';
      for(int j=0; j<n; j++) { // inner start => line start
