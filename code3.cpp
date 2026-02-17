ee2550ea460ac84c74f582f101bde7bd35e5efdb
Sourabh Farkare <sourabhfarkare73@gmail.com>
2026-02-17 14:01:51 +0530

    new

--- code3.cpp
+++ code3.cpp
@@ -5,8 +5,8 @@ int main() {
    cin>>n;
      for(int i=0; i<n; i++) { //outer
      char ch='A';
-     for(int j=0; j<n; j++) { // inner start => line start
-      cout <<ch ;
+     for(int j=0; j<i; j++) { // inner start => line start
+      cout <<ch<<" " ;
       ch= ch + 1;
      }
     cout << endl;
--- code6.cpp
+++ code6.cpp
@@ -5,7 +5,7 @@ int main() {
     int n=4;
     for(int i=0;i<n;i++){
         for(int j=0;j<i+1;j++){
-            cout<<(i+1);
+            cout<<(i+1)<<" ";
         }
         cout<<endl;
     }
