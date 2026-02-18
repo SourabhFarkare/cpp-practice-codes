c97e275eaa188cc4b7030cc5cca69a6e41982d11
Sourabh Farkare <sourabhfarkare73@gmail.com>
2026-02-18 15:59:12 +0530

    new

---
+++ 8_BubbleSort.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    cin>>arr[i];
+    //Bubble sort
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+    cout<<"sorted array through bubble sort"<<endl;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
